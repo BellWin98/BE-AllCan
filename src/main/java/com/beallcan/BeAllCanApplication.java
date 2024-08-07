c562fca077f66ec17e03935733f404e3da4e8cde
BellWin98 <jongseunghan@naver.com>
2024-08-07 13:29:12 +0900

    Initial Commit

---
+++ src/main/java/com/beallcan/BeAllCanApplication.java
@@ -0,0 +1,13 @@
+package com.beallcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAllCanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAllCanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beallcan/BeAllCanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beallcan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAllCanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
