b7b5667f847ab275c34272f19554835f830c2000
BellWin98 <jongseunghan@naver.com>
2024-08-09 16:55:10 +0900

    [Feat] AOP 적용
    * Controller, Service 단 호출 전 후로 로그 출력

---
+++ src/main/java/com/beallcan/global/aop/LogAop.java
@@ -0,0 +1,24 @@
+package com.beallcan.global.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAop {
+
+    // controller와 service method만 로그 적용
+    @Around("execution(* com.beallcan.domain..controller..*(..)) || execution(* com.beallcan.domain..service..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("START: {}", joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            log.info("END: {}", joinPoint.toString());
+        }
+    }
+}
