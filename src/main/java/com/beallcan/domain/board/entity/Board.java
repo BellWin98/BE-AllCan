00d5fc6c4b5194ea64429b3453071e60bf7a3569
BellWin98 <jongseunghan@naver.com>
2024-08-27 18:18:28 +0900

    [Feat] Board Entity 개발 중

---
+++ src/main/java/com/beallcan/domain/board/entity/Board.java
@@ -0,0 +1,30 @@
+package com.beallcan.domain.board.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column()
+    private String contents;
+
+    @Builder
+    public Board(String title, String contents){
+
+    }
+
+
+}
